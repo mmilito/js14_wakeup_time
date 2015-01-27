ef7a0638657a4d13525173538b8050990107e3ea
mmilito <militomariel@gmail.com>
2015-01-26 17:35:17 -0700

    EOD 1/26/15

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).on('ready', function() {
   
+var outerShell = $('<div class="outerShell"></div>');
+var innerShell = $('<div class="innerShell"></div>');
+var clockScreen = $('<div class="clockScreen"></div>');
+var clockText = $('<p class="clockText">0455</p>')
+
+
+//create outerShell
+$('.container').append(outerShell);
+$('.outerShell').append(innerShell);
+$('.innerShell').append(clockScreen);
+$('.clockScreen').append(clockText);
+
+
+// bottom of page
 });
